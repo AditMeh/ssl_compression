6d20eb28d7b15352beb3655d09350ec1a85902fd
AditMeh <aditmeh123@gmail.com>
2026-01-22 18:46:24 -0500

    scripts to run

--- mae_singlegpu/whatiran.sh
+++ mae_singlegpu/whatiran.sh
@@ -1,6 +1,15 @@
 python main_pretrain_singlegpu_embeddings.py \
-    --epochs 100 \
+    --epochs 200 \
     --embed_dim 512 \
-    --data_path ../moco-v3-singlegpu/imagenette2_tensors_128_dim/train/ \
+    --data_path ../moco-v3-singlegpu/embeddings/imagenette2_no_shared_initial_crop_512_dim/train/ \
     --num_patches 49 \
-    --wandb_name mae_pretrain_512_dim_imagenette
+    --wandb_name mae_pretrain_512_dim_imagenette_no_shared_initial_crop \
+    --output_dir ./output_dir/mae_pretrain_512_dim_imagenette_no_shared_initial_crop
+
+python main_pretrain_singlegpu_embeddings.py \
+    --epochs 200 \
+    --embed_dim 512 \
+    --data_path ../moco-v3-singlegpu/embeddings/imagenette2_shared_initial_crop_512_dim/train/ \
+    --num_patches 49 \
+    --wandb_name mae_pretrain_512_dim_imagenette_shared_initial_crop \
+    --output_dir ./output_dir/mae_pretrain_512_dim_imagenette_shared_initial_crop
--- moco-v3-singlegpu/run.sh
+++ moco-v3-singlegpu/run.sh
@@ -1,30 +1,44 @@
-python main_moco_single_gpu.py /datasets/imagenet/ \
-    -a resnet50 \
-    -b 256 \
-    --epochs 100 \
-    --workers 8 \
-    --crop-size 32 \
-    --wandb-project mocov3_singlegpu \
-    --wandb-name no_shared_initial_crop_moco \
-    --checkpoint-dir ./checkpoints_no_shared_initial_crop
+# python main_moco_single_gpu.py /datasets/imagenet/ \
+#     -a resnet50 \
+#     -b 256 \
+#     --epochs 100 \
+#     --workers 8 \
+#     --crop-size 32 \
+#     --wandb-project mocov3_singlegpu \
+#     --wandb-name no_shared_initial_crop_moco \
+#     --checkpoint-dir ./checkpoints_no_shared_initial_crop
 
-python main_moco_single_gpu.py ./imagenette2 \
-    -a resnet50 \
-    -b 256 \
-    --epochs 100 \
-    --workers 8 \
-    --crop-size 32 \
-    --wandb-project mocov3_singlegpu \
-    --wandb-name no_shared_initial_crop_moco \
-    --checkpoint-dir ./checkpoints_no_shared_initial_crop
+# python main_moco_single_gpu.py ./imagenette2 \
+#     -a resnet50 \
+#     -b 256 \
+#     --epochs 100 \
+#     --workers 8 \
+#     --crop-size 32 \
+#     --wandb-project mocov3_singlegpu \
+#     --wandb-name no_shared_initial_crop_moco \
+#     --checkpoint-dir ./checkpoints_no_shared_initial_crop
 
-python main_moco_single_gpu.py /datasets/imagenet/ \
+python main_moco_single_gpu.py ../datasets/imagenette2/ \
     -a resnet50 \
     -b 256 \
-    --epochs 100 \
+    --epochs 200 \
     --workers 8 \
     --crop-size 32 \
     --final-layer-planes 128 \
-    --wandb-project mocov3_singlegpu \
-    --wandb-name no_shared_initial_crop_moco_128_dim_imagenet \
-    --checkpoint-dir ./checkpoints_no_shared_initial_crop_128_dim_imagenet
+    --wandb-project fixed-size-compression-ssl \
+    --wandb-name shared_initial_crop_moco_512_dim_imagenette2 \
+    --checkpoint-dir ./checkpoints_shared_initial_crop_512_dim_imagenette2 \
+    --use-shared-initial-crop \
+    --seed 42
+
+python main_moco_single_gpu.py ../datasets/imagenette2/ \
+    -a resnet50 \
+    -b 256 \
+    --epochs 200 \
+    --workers 8 \
+    --crop-size 32 \
+    --final-layer-planes 128 \
+    --wandb-project fixed-size-compression-ssl \
+    --wandb-name no_shared_initial_crop_moco_512_dim_imagenette2 \
+    --checkpoint-dir ./checkpoints_no_shared_initial_crop_512_dim_imagenette2 \
+    --seed 42
